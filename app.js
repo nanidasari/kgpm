c50931c991532596f6cb415ef101991fac210d46
Isaac Dasari <90327970+nanidasari@users.noreply.github.com>
2022-08-03 17:39:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+<!-- jQuery -->
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script>
+   $(window).load(function() {
+   $(".preloader").delay(5000).fadeOut("slow");
+   $("#overlayer").delay(5000).fadeOut("slow");
+   })
+</script>
